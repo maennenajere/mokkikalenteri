a047358ecb9fd37a5a53192be92a27b018b32dc5
Jere Mäennenä <jere.maennena@gmail.com>
2024-04-25 03:47:35 +0300

    small fixes

--- public/app.js
+++ public/app.js
@@ -1,22 +1,26 @@
-// Your web app's Firebase configuration
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/performance';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase configuration
 const firebaseConfig = {
-	apiKey: 'AIzaSyC4cDmGioAZa4aaM64_bLDJFSOWeE6ZE4c',
-	authDomain: 'mokkikalenteri-9d64c.firebaseapp.com',
-	databaseURL:
-		'https://mokkikalenteri-9d64c-default-rtdb.europe-west1.firebasedatabase.app',
-	projectId: 'mokkikalenteri-9d64c',
-	storageBucket: 'mokkikalenteri-9d64c.appspot.com',
-	messagingSenderId: '670535971653',
-	appId: '1:670535971653:web:7cda674fda68f306b5042c',
-	measurementId: 'G-4XKSPL5TJK',
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
-// Initialize Firebase
+const { default: firebase } = require('firebase/compat/app');
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 const auth = firebase.auth();
+const perf = firebase.performance();
 
 const whenSignedIn = document.getElementById('whenSignedIn');
 const whenSignedOut = document.getElementById('whenSignedOut');
